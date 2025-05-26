7835c2bcfff03fa28ace23ef4c07d4d96d8f424c
govindongit <govindnag5863@gmail.com>
2025-05-26 23:59:59 +0530

    dynamic routes

---
+++ src/app/blog/products/page.tsx
@@ -0,0 +1,10 @@
+export default function products(){
+    return <>
+    <h1>Product Lists</h1>
+    <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+    </ul>
+    </>
+}
