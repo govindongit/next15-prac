159c26096aef912adcba6fa844f465b031fe67af
govindongit <govindnag5863@gmail.com>
2025-05-28 23:57:52 +0530

    change

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,4 +1,4 @@
 export default async function ProductDetails({params}:{params: Promise<{productId:string}>;}){
     const productId = (await params).productId;
-    return <h1>Details about the product {productId}</h1>;
+    return <h1>Details about the product {productId} inside the nested dynamic routes</h1>;
 }
